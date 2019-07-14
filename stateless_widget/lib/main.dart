b0ce6015b69c8aa070876bc9ddefa7fbdb7fcd53
PrashantP <prashant.patel@ngahr.com>
2019-07-14 13:25:27 +0100

    updates class name to StatelessApplication

--- stateless_widget/lib/main.dart
+++ stateless_widget/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new application());
+  runApp(new StatelessApplication());
 }
 
-class application extends StatelessWidget {
+class StatelessApplication extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
