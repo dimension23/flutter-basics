f1cb94d111708a3e2fdab8dd0b0d7a2e5823a72d
PrashantP <prashant.patel@ngahr.com>
2019-07-14 22:15:48 +0100

    row and column example in flutter

---
+++ row_and_column/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new RowAndColumn());
+
+class RowAndColumn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Row & Column',
+      home: new Scaffold(
+        appBar: new AppBar(
+            title: new Text(
+          'Row & Column',
+          style: TextStyle(fontFamily: 'Raleway'),
+        )),
+        body: new Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text('This'),
+            new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Text('This'),
+                new Text('is'),
+                new Text('Column'),
+              ],
+            ),
+            new Text('Row'),
+          ],
+        ),
+      ),
+    );
+  }
+}
