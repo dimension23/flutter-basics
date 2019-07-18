9957344d520a9ce7c1399e800c9816b9178f799c
PrashantP <prashant.patel@ngahr.com>
2019-07-18 22:19:11 +0100

    textfield widget

---
+++ textfield_widget/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(TextFieldWidget());
+
+class TextFieldWidget extends StatefulWidget {
+  @override
+  _TextFieldWidgetState createState() => _TextFieldWidgetState();
+}
+
+class _TextFieldWidgetState extends State<TextFieldWidget> {
+  String ptext = '';
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('TextWidget'),
+        ),
+        body: new Column(
+          children: <Widget>[
+            new TextField(
+              onSubmitted: (String txt) {
+                setState((){
+                  ptext = txt;
+                });
+              },
+              decoration: new InputDecoration(hintText: 'Email Address', labelText: 'User Id'),
+            ),
+            new Padding(padding: const EdgeInsets.all(10.0)),
+            new Text(ptext),
+          ],
+        ),
+      ),
+    );
+  }
+}
